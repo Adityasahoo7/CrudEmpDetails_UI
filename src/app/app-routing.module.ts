4c49190a3d8734dfac18c652de6fb02c7dde0f5e
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-02-17 12:29:00 +0530

    Create Folder Structer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { AddEmployeeComponent } from './features/employee/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './features/employee/edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './features/employee/view-employee/view-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'edit-employee/:id', component: EditEmployeeComponent },
+  { path: 'view-employee/:id', component: ViewEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { AddEmployeeComponent } from './features/employee/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './features/employee/edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './features/employee/view-employee/view-employee.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    ViewEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
